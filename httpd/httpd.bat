077f19fd6ad03ca663b6277337d09f1a4a65a1ba
thinkbase <thinkbase.net@gmail.com>
2012-11-11 00:13:15 +0800

    发现造成 C Run-Time Error R6034 问题的原因在 PIL 库，在安装 PortablePython 时不安装即可解决

--- httpd/httpd.bat
+++ httpd/httpd.bat
@@ -6,6 +6,12 @@ set VIRTUAL_DISK=B
 subst %VIRTUAL_DISK%: /D > NUL
 subst %VIRTUAL_DISK%: "%PORTABLE_HOME%\httpd"
 
+echo.
+echo ---^> Start httpd:
+echo .     - at virtaul driver "%VIRTUAL_DISK%:"
+echo .     - started in "%CD%"
+echo.
+echo ---^> PATH: %PATH%
 @echo on
 call "%PORTABLE_HOME%\httpd\Apache2.2\bin\httpd.exe"
 @echo off
