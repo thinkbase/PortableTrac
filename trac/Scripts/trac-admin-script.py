f9321e9a7b9208a910ea204fcaf8f05cb9fa73d7
thinkbase <thinkbase.net@gmail.com>
2013-02-18 14:42:43 +0800

    20130218 - upgrade to Trac 1.0.1:   >>> easy_install --upgrade Trac==1.0.1

--- trac/Scripts/trac-admin-script.py
+++ trac/Scripts/trac-admin-script.py
@@ -1,9 +1,9 @@
-#!"E:\PortableTrac\Portable Python 2.7.3.1\App\python.exe"
-# EASY-INSTALL-ENTRY-SCRIPT: 'trac==1.0','console_scripts','trac-admin'
-__requires__ = 'trac==1.0'
+#!"I:\thinkbase.net\github\PortableTrac-git\Portable Python 2.7.3.1\App\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'Trac==1.0.1','console_scripts','trac-admin'
+__requires__ = 'Trac==1.0.1'
 import sys
 from pkg_resources import load_entry_point
 
 sys.exit(
-   load_entry_point('trac==1.0', 'console_scripts', 'trac-admin')()
+   load_entry_point('Trac==1.0.1', 'console_scripts', 'trac-admin')()
 )
